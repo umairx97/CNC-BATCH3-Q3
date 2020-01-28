8bf726508935343e8d91003d67be3d6c9c861282
umairx97 <umair.bajwa@hotmail.com>
2020-01-28 16:55:58 +0500

    update topic 27Jan

---
+++ 01-Advance-JS/27Jan/destructuringExample.js
@@ -0,0 +1,101 @@
+//DESTRUCTURING ARRAY
+
+/************************************************** */
+// Try any part one at a time
+
+
+let myArray = ["Umair","Ahmed",2]
+
+let [firstName, lastName, degrees] = myArray
+
+console.log(firstName)
+console.log(lastName)
+console.log(degrees)
+
+let sentence = `${firstName} ${lastName} has ${degrees} Masters degrees.`
+
+console.log(sentence)
+// RESULT:
+// Umair
+// Ahmed
+// 2
+// Umair Ahmed has 2 Masters degrees.
+
+
+
+
+// /************************************************** */
+// let myArray = ["Umair","Ahmed",2]
+
+// let [firstName, lastName] = myArray
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(degrees)
+
+// let sentence = `${firstName} ${lastName} has ${degrees} Masters degrees.`
+
+// console.log(sentence)
+// // RESULT:
+// // Umair
+// // Ahmed
+// // degrees is not defined
+
+
+
+
+
+
+
+// /************************************************** */
+//DESTRUCTURING OBJECT
+
+
+// let myObject = {firstName: "Umair", lastName: "Ahmed", degrees: 2}
+
+// console.log(myObject.firstName)
+// console.log(myObject.lastName)
+// console.log(myObject.degrees)
+
+// let {firstName, lastName, degrees} = myObject
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(degrees)
+
+// let sentence = `${firstName} ${lastName} has ${degrees} Masters degrees`
+
+// console.log(sentence)
+// Umair
+// Ahmed
+// 2
+// Umair
+// Ahmed
+// 2
+// Umair Ahmed has 2 Masters degrees.
+
+
+
+
+
+
+
+// /************************************************** */
+
+
+// let myObject = {firstName: "Umair", lastName: "Ahmed", degrees: 2}
+
+// console.log(myObject.firstName)
+// console.log(myObject.lastName)
+// console.log(myObject.degrees)
+
+// let {firstName, lastName} = myObject
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(degrees)
+
+// let sentence = `${firstName} ${lastName} has ${degrees} Masters degrees`
+
+// console.log(sentence)
+
---
+++ 01-Advance-JS/27Jan/variableHoistingExample.js
@@ -0,0 +1,30 @@
+//VARIABLE HOISTING
+// Try any part one at a time
+
+var myname;         // Declare Variable
+myname = 'Umair';   // Initialize variable
+console.log(myname) // result: Umair
+
+
+
+/*********************************** */
+
+// var myname;          // Declare Variable
+// console.log(myname)  // result: Undefined
+// myname = 'Umair';    // Initialize variable
+
+
+
+/*********************************** */
+
+// console.log(myname)  // result: Undefined
+// myname = 'Umair';    // Initialize variable
+// var myname;          // Declare Variable
+
+
+
+/*********************************** */
+
+// console.log(myname)  // result: ERROR: myname is not defined
+//myname = 'Umair';     // Initialize variable
+
